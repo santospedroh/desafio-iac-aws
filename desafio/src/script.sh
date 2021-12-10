11c7d79103e7d8d67e28799c297950da1cb5faea
Pedro Santos <pedsantos@uolinc.com>
2021-12-09 21:03:16 -0300

    zip groovin

--- desafio/src/script.sh
+++ desafio/src/script.sh
@@ -3,8 +3,9 @@
 sudo yum update -y
 sudo amazon-linux-extras install -y nginx1.12 -y
 sudo systemctl start nginx -y
+sudo systemctl enable nginx
 
 ## Install Site
-sudo unzip /tmp/groovin.zip -d /usr/share/nginx/html/
-sudo mv /usr/share/nginx/html/Groovin/* /usr/share/nginx/html/
+sudo unzip /tmp/groovin.zip
+sudo mv /tmp/Groovin/* /usr/share/nginx/html/
 sudo systemctl restart nginx
