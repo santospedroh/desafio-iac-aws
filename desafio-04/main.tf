8b72e893567f50eeb6af285ce8bab3ae5b18b4e2
Pedro Santos <pedsantos@uolinc.com>
2021-12-27 14:06:27 -0300

    edit autoscaling group

--- desafio-04/main.tf
+++ desafio-04/main.tf
@@ -107,24 +107,6 @@ module "ec2_jump" {
     }
 }
 
-module "ec2_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 3.0"
-
-  for_each = toset(["0", "1"])
-
-  name                   = "${var.instance_name}-${each.key}"
-  ami                    = var.instance_ami
-  instance_type          = var.instance_type
-  key_name               = "vockey"
-  monitoring             = true
-  vpc_security_group_ids = [aws_security_group.sg-instance-name.id]
-  subnet_id              = module.vpc.private_subnets[each.key]
-  user_data     = file("./src/flappy-bird.sh")
-
-  tags = var.instance_tags
-}
-
 #Aplication Load Balancer
 module "alb" {
   source  = "terraform-aws-modules/alb/aws"
@@ -153,16 +135,6 @@ module "alb" {
         protocol            = "HTTP"
         matcher             = "200-399"
       }
-      targets = [
-        {
-          target_id = module.ec2_instance[0].id
-          port = 80
-        },
-        {
-          target_id = module.ec2_instance[1].id
-          port = 80
-        }
-      ]
     }
   ]
 
@@ -182,24 +154,42 @@ resource "aws_launch_configuration" "as_conf" {
   name_prefix   = "lc-fb-"
   image_id      = var.instance_ami
   instance_type = var.instance_type
+  security_groups = [aws_security_group.sg-instance-name.id]
   key_name      = "vockey"
   user_data     = file("./src/flappy-bird.sh")
 
   lifecycle {
     create_before_destroy = true
   }
+
 }
 
 # Auto Scaling
 resource "aws_autoscaling_group" "asg-flappy-bird" {
   name                 = "asg-flappy-bird"
   launch_configuration = aws_launch_configuration.as_conf.name
-  availability_zones   = [module.vpc.private_subnets[0].id, module.vpc.private_subnets[1].id]
-  min_size             = 1
-  max_size             = 2
+  vpc_zone_identifier  = [module.vpc.private_subnets[0], module.vpc.private_subnets[1]]
+  health_check_grace_period = 90
+  health_check_type    = "ELB"
+  force_delete         = true
+  desired_capacity     = 2
+  min_size             = 2
+  max_size             = 3
 
   lifecycle {
     create_before_destroy = true
   }
+  tag {
+    key                 = "Name"
+    value               = "flappy-bird-asg"
+    propagate_at_launch = true
+  }
+
+}
+
+# Create a new ALB Target Group attachment
+resource "aws_autoscaling_attachment" "asg_attachment" {
+  autoscaling_group_name = aws_autoscaling_group.asg-flappy-bird.id
+  alb_target_group_arn   = module.alb.target_group_arns[0]
 }
 
